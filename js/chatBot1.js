518c238bda2a2166a34f29aa241c6fc92b6d4530
Ashiq Cherian <67139570+ninjahurricane007@users.noreply.github.com>
2023-11-27 09:05:26 +0530

    Update chatBot1.js

--- js/chatBot1.js
+++ js/chatBot1.js
@@ -3,11 +3,11 @@ const closeBtn = document.querySelector(".close-btn");
 const chatbox = document.querySelector(".chatbox");
 const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
-
+ 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "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"; // Paste your API key here
+const API_KEY = "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"; // Paste your API key here
 const inputInitHeight = chatInput.scrollHeight;
-
+ 
 const createChatLi = (message, className) => {
   // Create a chat <li> element with passed message and className
   const chatLi = document.createElement("li");
@@ -20,46 +20,15 @@ const createChatLi = (message, className) => {
   chatLi.querySelector("p").textContent = message;
   return chatLi; // return chat <li> element
 };
-
-// const generateResponse = (chatElement) => {
-//   const API_URL = "https://api.openai.com/v1/chat/completions";
-//   const messageElement = chatElement.querySelector("p");
-
-//   // Define the properties and message for the API request
-//   const requestOptions = {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//       Authorization: `Bearer ${API_KEY}`,
-//     },
-//     body: JSON.stringify({
-//       model: "gpt-3.5-turbo",
-//       messages: [{ role: "user", content: userMessage }],
-//     }),
-//   };
-
-//   // Send POST request to API, get response and set the reponse as paragraph text
-//   fetch(API_URL, requestOptions)
-//     .then((res) => res.json())
-//     .then((data) => {
-//       messageElement.textContent = data.choices[0].message.content.trim();
-//     })
-//     .catch(() => {
-//       messageElement.classList.add("error");
-//       messageElement.textContent =
-//         "Oops! Something went wrong. Please try again.";
-//     })
-//     .finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
-// };
-
+ 
 const generateResponse = async (chatElement) => {
   const API_URL = "https://api.openai.com/v1/chat/completions";
   const messageElement = chatElement.querySelector("p");
-
+ 
   try {
-    // Your Starbucks-related prompt
+    // my Starbucks-related prompt
     const starbucksPrompt = `Chat with the users as a Starbucks bot: ${userMessage}`;
-
+ 
     // Send a POST request to the OpenAI API
     const response = await fetch(API_URL, {
       method: "POST",
@@ -75,12 +44,12 @@ const generateResponse = async (chatElement) => {
         ],
       }),
     });
-
+ 
     const data = await response.json();
-
+ 
     // Extract the message from the API response
     const botMessage = data.choices[0].message.content.trim();
-
+ 
     // Set the response as paragraph text
     messageElement.textContent = botMessage;
   } catch (error) {
@@ -91,19 +60,19 @@ const generateResponse = async (chatElement) => {
     chatbox.scrollTo(0, chatbox.scrollHeight);
   }
 };
-
+ 
 const handleChat = () => {
   userMessage = chatInput.value.trim(); // Get user entered message and remove extra whitespace
   if (!userMessage) return;
-
+ 
   // Clear the input textarea and set its height to default
   chatInput.value = "";
   chatInput.style.height = `${inputInitHeight}px`;
-
+ 
   // Append the user's message to the chatbox
   chatbox.appendChild(createChatLi(userMessage, "outgoing"));
   chatbox.scrollTo(0, chatbox.scrollHeight);
-
+ 
   setTimeout(() => {
     // Display "Thinking..." message while waiting for the response
     const incomingChatLi = createChatLi("Thinking...", "incoming");
@@ -112,13 +81,13 @@ const handleChat = () => {
     generateResponse(incomingChatLi);
   }, 600);
 };
-
+ 
 chatInput.addEventListener("input", () => {
   // Adjust the height of the input textarea based on its content
   chatInput.style.height = `${inputInitHeight}px`;
   chatInput.style.height = `${chatInput.scrollHeight}px`;
 });
-
+ 
 chatInput.addEventListener("keydown", (e) => {
   // If Enter key is pressed without Shift key and the window
   // width is greater than 800px, handle the chat
@@ -127,7 +96,7 @@ chatInput.addEventListener("keydown", (e) => {
     handleChat();
   }
 });
-
+ 
 sendChatBtn.addEventListener("click", handleChat);
 closeBtn.addEventListener("click", () =>
   document.body.classList.remove("show-chatbot")
